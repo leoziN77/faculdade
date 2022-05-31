0a409d24c414b017b8fda5b021257e83f890833f
leoziN77 <99442194+leoziN77@users.noreply.github.com>
2022-05-31 18:26:27 +0000

    Tue, May 31, 2022, 6:26 PM +00:00

---
+++ 5/agree.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    chat c = get_char("Você condorda?);
+    if (c = "y");
+    {
+        printf("Concordo\n");
+    }
+    else if (c = "n")
+    {
+        printf("Não concordo\n");
+    }
+    
