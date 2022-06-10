eb9dbd9cf7ef9cf7d83c07a0582d1cf6c60b47a2
leoziN77 <99442194+leoziN77@users.noreply.github.com>
2022-06-10 14:26:01 +0000

    Fri, Jun 10, 2022, 2:26 PM +00:00

--- 6/mario.c
+++ 6/mario.c
@@ -3,5 +3,14 @@
 
 int main(void)
 {
-    //
+    //Número inteiro escrito pelo usuario
+    int n;
+    do
+    {
+        n = get_int("Width: ");
+    }
+    while (n < 1);
+
+    //Imprimir x pontos de interrogação
+    for (int i = 0; i < n; i++)
 }
