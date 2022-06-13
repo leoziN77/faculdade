e0ec29ec1c59e670a20e32d42f1cfb70a8fbd621
leoziN77 <99442194+leoziN77@users.noreply.github.com>
2022-06-13 14:52:32 +0000

    Mon, Jun 13, 2022, 2:52 PM +00:00

--- 0/mario.c
+++ 0/mario.c
@@ -23,6 +23,11 @@ int main(void)
         {
             printf("#");
         }
+        printf("  ");
+        for  (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
         printf("\n");
     }
 }
