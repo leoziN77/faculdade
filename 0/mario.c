469ab2d1c06518e8548abdd6cc54df9d51aec8e7
leoziN77 <99442194+leoziN77@users.noreply.github.com>
2022-06-13 14:00:20 +0000

    Mon, Jun 13, 2022, 2:00 PM +00:00

--- 0/mario.c
+++ 0/mario.c
@@ -13,13 +13,13 @@ int main(void)
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
-        {
-            if (i + j < n - 1)
-                printf(" ");
-            else
+        //Blankspaces
+        for (int j = n - 1; j >= 0; j--)
+            printf(" ");
+
+        //Hashtag
+        for (int j = 0; j <= i; i++)
             printf("#");
-        }
         printf("\n");
     }
 }
