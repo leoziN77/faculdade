9cadab421f0cedb0a94274ccc5566f48770c91a3
leoziN77 <99442194+leoziN77@users.noreply.github.com>
2023-03-20 17:26:17 +0000

    Mon, Mar 20, 2023, 5:26 PM +00:00

---
+++ 0/maiorque.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    float n;
+
+    printf("Insira o valor: ");
+    scanf("%f", &n);
+
+    if (n >= '20');
+    {
+        printf("Maior que 20!\n");
+    }
+
+    return 0;
+}
