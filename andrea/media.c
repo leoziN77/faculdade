e6951c6a9bae1609f4e04b01273f361e25a61a4e
leoziN77 <99442194+leoziN77@users.noreply.github.com>
2023-03-21 18:12:34 +0000

    Tue, Mar 21, 2023, 6:12 PM +00:00

---
+++ andrea/media.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    float p1, p2, media;
+
+    printf("Digite sua primeira nota: ");
+    scanf("%f", &p1);
+
+    printf("Digite sua segunda nota: ");
+    scanf("%f", &p2);
+
+    media = (p1+p2)/2;
+
+    if(media >= 5)
+    {
+        printf("Aprovado!\n", media);
+    }
+
+    return 0;
+}    
