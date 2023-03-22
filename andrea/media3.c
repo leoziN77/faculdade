725d8de97a4c6d435f72e39a21be1b512e8be2ef
leoziN77 <99442194+leoziN77@users.noreply.github.com>
2023-03-22 00:16:21 +0000

    Wed, Mar 22, 2023, 12:16 AM +00:00

--- andrea/media3.c
+++ andrea/media3.c
@@ -2,4 +2,26 @@
 #include <stdlib.h>
 #include <locale.h>
 
-int main(){}
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    float media;
+
+    printf("Digite a média: ");
+    scanf("%f", &media);
+
+    if(media > 5)
+    {
+        printf("Aprovado!\n");
+    }
+    else
+    {
+        if(media < 3)
+        {
+            printf("Reprovado!");
+        }
+        else
+        {
+            printf("Exame");
+        }
+    }
+}
