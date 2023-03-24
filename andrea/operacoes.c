f29921d6b72eb668c51de25c17a50872abb342c1
leoziN77 <99442194+leoziN77@users.noreply.github.com>
2023-03-24 12:54:53 +0000

    Fri, Mar 24, 2023, 12:54 PM +00:00

--- andrea/operacoes.c
+++ andrea/operacoes.c
@@ -18,7 +18,7 @@ int main(){
 	printf("*                               *\n");
 	printf("*********************************\n");
 
-	printf("\nAgora escolha a opção desejada: ");
+	printf("\nEscolha a opção desejada: ");
 	scanf("%c", &opcao);
 
 	printf("Digite o primeiro número: ");
@@ -27,35 +27,35 @@ int main(){
 	printf("Digite o segundo número: ");
 	scanf("%f", &numero2);
 
-	if(opcao == 'A')
+	if(opcao == 'A' || opcao == 'a')
 	{
 		resultado = numero1 + numero2;
-		printf("O resultado da soma é %.2f\n", resultado);
+		printf("O resultado da soma é: %.2f\n", resultado);
 	}
 	else
 	{
-		if(opcao == 'B')
+		if(opcao == 'B' || opcao == 'b')
 		{
 			resultado = numero1 * numero2;
-			printf("O resultado da multiplicação é %.2f\n", resultado);
+			printf("O resultado da multiplicação é: %.2f\n", resultado);
 		}
 		else
 		{
-			if(opcao == 'C')
+			if(opcao == 'C' || opcao == 'c')
 			{
 				resultado = numero1 - numero2;
-				printf("O resultado da subtração é %.2f\n", resultado);
+				printf("O resultado da subtração é: %.2f\n", resultado);
 			}
 			else
 			{
-				if(opcao == 'D')
+				if(opcao == 'D' || opcao == 'd')
 				{
 					resultado = numero1 / numero2;
-					printf("O resultado da divisão é %.2f\n", resultado);
+					printf("O resultado da divisão é: %.2f\n", resultado);
 				}
 				else
 				{
-					printf("Comando invalido!\n");
+					printf("Opção escolhida invalida!\n");
 				}
 			}
 		}
