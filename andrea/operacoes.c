6eb0df71401736c60c3dfd0639ada74c9da4382b
leoziN77 <99442194+leoziN77@users.noreply.github.com>
2023-03-24 12:38:35 +0000

    Fri, Mar 24, 2023, 12:38 PM +00:00

---
+++ andrea/operacoes.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	float numero1, numero2, resultado;
+	char opcao;
+
+	printf("\n*********************************\n");
+	printf("*                               *\n");
+	printf("*          Menu de opções       *\n");
+	printf("*                               *\n");
+	printf("*  A- Somar dois números        *\n");
+	printf("*  B- Multiplicar dois números  *\n");
+	printf("*  C- Subtrair dois números     *\n");
+	printf("*  D- Dividir dois números      *\n");
+	printf("*                               *\n");
+	printf("*********************************\n");
+
+	printf("\nAgora escolha a opção desejada: ");
+	scanf("%c", &opcao);
+
+	printf("Digite o primeiro número: ");
+	scanf("%f", &numero1);
+
+	printf("Digite o segundo número: ");
+	scanf("%f", &numero2);
+
+	if(opcao == A)
+	{
+		resultadp = numero1 + numero2
+		printf("O resultado da soma é %.2f", resultado);
+	}
+	else
+	{
+		if(opcao == B)
+		{
+			resultado = numero1 * numero2
+			printf("O resultado da multiplicação é %.2f", resultado);
+		}
+	}
+	return 0;
+}
