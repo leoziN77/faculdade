98a29845c733b2cfb1d4e4dcde640e95d4f0b6eb
leoziN77 <99442194+leoziN77@users.noreply.github.com>
2023-04-19 00:02:19 +0000

    Wed, Apr 19, 2023, 12:02 AM +00:00

---
+++ andrea/registro4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+struct st_banca {
+	char titulo[20];
+	int quantidade;
+	float custo;
+	float custo em estoque;
+};
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+
+	struct st_banca banca;
+
+
+
+
+
+
+	return 0;
+}
