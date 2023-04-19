1a1f290ee42f8dc77b3f7e51d7b0c5884a78bd91
leoziN77 <99442194+leoziN77@users.noreply.github.com>
2023-04-19 04:13:31 +0000

    Wed, Apr 19, 2023, 4:13 AM +00:00

--- andrea/registro4.c
+++ andrea/registro4.c
@@ -13,40 +13,50 @@ int main(){
     setlocale(LC_ALL, "Portuguese");
     struct st_registro registro;
     int r, i=1;
-    float custoestoque, menor = 10000;
+    double custoestoque, menor = 10000;
     char tb[20]; //titulo menor
 
     printf("Quantos títulos deseja cadastrar: ");
     scanf("%d", &r);
+
+    if (r <= 0) {
+        printf("Quantidade inválida. O programa será encerrado.\n");
+        return 1;
+    }
+
     printf("-------------------------------------------------------\n");
-        fflush(stdin);
+    	fflush(stdin); // lê e descarta o caractere de nova linha deixado no buffer pelo scanf
     do{
         printf("Qual o título da revista ou do jornal: ");
-        gets(registro.titulo);
+        fgets(registro.titulo, sizeof(registro.titulo), stdin);
         printf("Quantidade de revistas ou jornais em estoque: ");
         scanf("%d", &registro.quantidade);
         printf("Custo unitário do título: R$");
         scanf("%f", &registro.custo);
-            fflush(stdin);
-            custoestoque = registro.custo*registro.quantidade;
-                system("cls");
-            printf("Revista: %d\n", 1);
-            printf("-------------------------------------------------------\n");
-            printf("Título: %s\n", registro.titulo);
-            printf("Quantidade: %d\n", registro.quantidade);
-            printf("Custo unitário: R$%.2f\n", registro.custo);
-            printf("Custo em estoque: R$%.2f\n", custoestoque);
-            printf("-------------------------------------------------------\n");
+        fflush(stdin); // lê e descarta o caractere de nova linha deixado no buffer pelo scanf
 
-            if(registro.custo < menor){
-                menor = registro.custo;
-                strcpy(tb, registro.titulo);
-            }
-            i++;
-    }
-    while (i <= r);
+        custoestoque = registro.custo * registro.quantidade;
+
+        system("cls");
+
+        printf("Revista: %d\n", i);
+        printf("-------------------------------------------------------\n");
+        printf("Título: %s\n", registro.titulo);
+        printf("Quantidade: %d\n", registro.quantidade);
+        printf("Custo unitário: R$%.2f\n", registro.custo);
+        printf("Custo em estoque: R$%.2f\n", custoestoque);
+        printf("-------------------------------------------------------\n");
+
+        if(registro.custo < menor){
+            menor = registro.custo;
+            strcpy(tb, registro.titulo);
+        }
+        i++;
+    } while (i <= r);
+
     printf("-------------------------------------------------------\n");
     printf("O título mais barato é: %s", tb);
 
     return 0;
 }
+
