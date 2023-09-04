6800a197bacbd5fba5813c1b50088bdb484b24c9
Leonardo Chiquito <99442194+leoziN77@users.noreply.github.com>
2023-09-04 20:44:43 -0300

    horas.c

--- andrea/horas.c
+++ andrea/horas.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL,"portuguese");
+    int horas, minutos, minutos_final;
+
+    printf("*******************************\n");
+    printf("*                             *\n");
+    printf("*     Contador de minutos     *\n");
+    printf("*                             *\n");
+    printf("*       1 hora = 60 minutos   *\n");
+    printf("*******************************\n");
+
+    do {
+        printf("Digite somente as horas: ");
+        scanf("%d", &horas);
+
+        if (horas < 0 || horas > 24) {
+            printf("Valor inválido! As horas devem estar entre 0 e 24.\n");
+        }
+    } while (horas < 0 || horas > 24);
+
+    do {
+        printf("Digite somente os minutos: ");
+        scanf("%d", &minutos);
+
+        if (minutos < 0 || minutos >= 60) {
+            printf("Valor inválido! Os minutos devem estar entre 0 e 59.\n");
+        }
+    } while (minutos < 0 || minutos >= 60);
+
+    minutos_final = 60 * horas + minutos;
+
+    printf("Em %d horas e %d minutos, temos um total de %d minutos.\n", horas, minutos, minutos_final);
+
+    return 0;
+}
 
