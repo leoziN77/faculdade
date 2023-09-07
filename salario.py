688b4b00164c6eff6abcb2dcdfea5e4a65f11a66
Chiquin <99442194+leoziN77@users.noreply.github.com>
2023-09-07 18:10:54 -0300

    Delete salario.py

--- salario.py
+++
@@ -1,7 +0,0 @@
-valor_salario_minimo = 1320.00
-
-salario_profissional = float(input("Digite o salário do profissional: "))
-
-quantidade_salarios_minimos = salario_profissional / valor_salario_minimo
-
-print(f"O profissional recebe aproximadamente {quantidade_salarios_minimos:.2f} salários mínimos.")
