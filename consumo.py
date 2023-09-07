c6497f647f0e8bb1674836191240a1dc5291dd8b
Chiquin <99442194+leoziN77@users.noreply.github.com>
2023-09-07 18:07:36 -0300

    Delete consumo.py

--- consumo.py
+++
@@ -1,6 +0,0 @@
-distancia_percorrida = float(input("Digite a distância total percorrida em KM: "))
-volume_combustivel = float(input("Digite o volume de combustível consumido em litros: "))
-
-consumo_medio = distancia_percorrida / volume_combustivel
-
-print(f"O consumo médio do automóvel é de {consumo_medio:.2f} km/l.")
