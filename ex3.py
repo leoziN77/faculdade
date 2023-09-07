adbc9b142df1699ca0fe75aa356b5bd851511aa3
Chiquin <99442194+leoziN77@users.noreply.github.com>
2023-09-07 18:09:19 -0300

    Delete ex3.py

--- ex3.py
+++
@@ -1,9 +0,0 @@
-import math
-
-numero = int(input("Digite um número inteiro: "))
-
-if numero < 0:
-    print("Valor inválido. Não é possível calcular a raiz quadrada de um número negativo.")
-else:
-    raiz_quadrada = math.sqrt(numero)
-    print(f"A raiz quadrada de {numero} é {raiz_quadrada:.2f}")
