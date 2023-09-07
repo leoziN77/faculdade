540b48dfac4aefb51b026fb52fdab4b2dacd9538
Chiquin <99442194+leoziN77@users.noreply.github.com>
2023-09-07 18:08:53 -0300

    Delete ex14.py

--- ex14.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input("Digite o primeiro número inteiro: "))
-num2 = int(input("Digite o segundo número inteiro: "))
-num3 = int(input("Digite o terceiro número inteiro: "))
-
-maior = max(num1, num2, num3)
-
-print(f"O maior número é: {maior}")
